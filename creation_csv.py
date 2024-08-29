24afb61195deb82506504c593a9fab607583ab98
Pumkpin <badoalan@gmail.com>
2024-08-29 19:10:47 +0200

    modification fichiers

--- creation_csv.py
+++ creation_csv.py
@@ -4,7 +4,7 @@ def impression(infos, nom, chemin) :
     en_tete = ["product_page_url", "universal_ product_code(upc)", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image"]
 
     chemin_fichier = chemin + "/" + nom + ".csv"
-    with open(chemin_fichier, "a") as f :
+    with open(chemin_fichier, "w") as f :
         writer = csv.writer(f, delimiter = ",")
         writer.writerow(en_tete)
         for info in infos :

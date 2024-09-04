feec44c594293dba5a69a70e366b550b0d7e4eea
Pumkpin <badoalan@gmail.com>
2024-09-04 13:50:05 +0200

    modification de  description livre et categorys

--- infos_livres.py
+++ infos_livres.py
@@ -36,31 +36,32 @@ def all_infos_livre(url) :
 
     donnees_livre = []
     for j in link_book :
-
-        description_livre =[]
-        descriptions = Soup(j).find_all("p")
-        for desc in descriptions:
-           description_livre.append(desc.string)
-        description = description_livre[3]
+        #Récupération de la description du livre
+        descriptions = Soup(j).find("div", class_="sub-header")
+        description_livre = descriptions.find_next("p").text
         
+        #Récupération du UPC, des prix, du nombre en stock et du range
         info_livre = []
         infos = Soup(j).find_all("td")
         for info in infos :
             info_livre.append(info.string)
 
+        #Récupération et si besoin, modification du titre
         titre = Soup(j).find("h1")
         t_l = titre.string
         titre_livre = t_l.replace("/", " ")
 
-        categorys = []
-        category = Soup(j).find_all("a")
-        for cat in category :
-          categorys.append(cat.string)
-        catagory_livre = categorys[3]
+        #Récupération du nom de la catégorie du livre
+        category = Soup(j).find("ul", class_="breadcrumb")
+        cat = category.find_all("li")
+        catagory_livre = cat[2].text.strip()
 
+        #Récupération de l'URL de l'image du livre
         image = Soup(j).find("img")
         image_couverture = urljoin("http://books.toscrape.com/", image.get("src"))
 
+        #Création d'un dossier image et téléchargement de l'image dans le dossier
+        #L'image est également renomé en fonction du nom du livre.
         if not exists("images"):
             os.mkdir("images")
         nom_du_fichier = titre_livre + ".jpg"
@@ -71,8 +72,6 @@ def all_infos_livre(url) :
         f.write(response.content)
         f.close()
 
-        
-
-        desc_livre = [j, info_livre[0],titre_livre, info_livre[3], info_livre[2], info_livre[5], description_livre[3], catagory_livre, info_livre[6], image_couverture, chemin_image]
+        desc_livre = [j, info_livre[0],titre_livre, info_livre[3], info_livre[2], info_livre[5], description_livre, catagory_livre, info_livre[6], image_couverture, chemin_image]
         donnees_livre.append(desc_livre)
     return donnees_livre

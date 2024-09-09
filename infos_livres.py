e49074c203f85d4e7971c54212384631a621cb3c
Pumkpin <badoalan@gmail.com>
2024-09-09 17:52:45 +0200

    Creation d'un dictionnaire pour donnees_livres, separation des parties Recuperation et Transformation

--- infos_livres.py
+++ infos_livres.py
@@ -1,68 +1,134 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-import os
-from creation_csv import telechargement
 
-def all_infos_livre(url, chemin_images) :
+
+def extract_data_books(url) :
     """
     Fonction qui prend en paramètre un lien url.
     Elle va scrap des données via le lien.
-    Elle appelle la fonction de téléchargement qui télécharge et enregistre les images dans un dossier dédié.
     """
-
     donnees_livre = []
-    new_link = []
+    list_link = []
     link_book = []
-    new_link.append(url)
+
+    list_link.append(url)
     page = requests.get(url)
     soupe = BeautifulSoup(page.content, "html.parser")
+
+    # Vérification du nombre de pages de la catégorie.
     pages = soupe.find("li", class_="next")
     while pages != None :
         next_page = pages.a.get("href")
         url_modif = url.replace("index.html", next_page)
-        new_link.append(url_modif)
+        list_link.append(url_modif)
         page_modif = requests.get(url_modif)
-        soup = BeautifulSoup(page_modif.content, "html.parser")
-        pages = soup.find("li", class_="next")
+        soupe = BeautifulSoup(page_modif.content, "html.parser")
+        pages = soupe.find("li", class_="next")
     
-    for next_page in new_link :
+    # Création d'une boucle sur les liens pour récupérer les données.
+    for next_page in list_link :
         page = requests.get(next_page)
         link = BeautifulSoup(page.content, "html.parser")
         links = link.find_all("article")
         for i in links :
             link_book = (i.h3.a.get("href").replace("../../..", "http://books.toscrape.com/catalogue"))
             page = requests.get(link_book)
-            s = BeautifulSoup(page.content, "html.parser")    
-            #Récupération de la description du livre
-            descriptions = s.find("div", class_="sub-header")
+            soup = BeautifulSoup(page.content, "html.parser")
+
+            # Récupération de la description du livre
+            descriptions = soup.find("div", class_="sub-header")
             description_livre = descriptions.find_next("p").text
         
-            #Récupération du UPC, des prix, du nombre en stock et du range
+            # Récupération du UPC, des prix et du nombre en stock
             info_livre = []
-            infos = s.find_all("td")
+            infos = soup.find_all("td")
             for info in infos :
                 info_livre.append(info.string)
 
-            #Récupération et si besoin, modification du titre
-            titre = s.find("h1")
-            t_l = titre.string
-            titre_livre = t_l.replace("/", " ")
+            # Récupération du titre
+            titre = soup.find("h1")
+            titre_livre = titre.string
 
-            #Récupération du nom de la catégorie du livre
-            category = s.find("ul", class_="breadcrumb")
+            # Récuperation de la note du livre
+            rating = soup.find("p", class_="star-rating")
+            book_rating = rating["class"][-1]
+
+            # Récupération du nom de la catégorie du livre
+            category = soup.find("ul", class_="breadcrumb")
             cat = category.find_all("li")
             catagory_livre = cat[2].text.strip()
 
-            #Récupération de l'URL de l'image du livre
-            image = s.find("img")
+            # Récupération de l'URL de l'image du livre
+            image = soup.find("img")
             image_couverture = urljoin("http://books.toscrape.com/", image.get("src"))
         
+            # Regroupement des données dans un dictionaire
+            donnees_livre.append({
+                "product_page_url" : link_book,
+                "universal_product_code (upc)" : info_livre[0],
+                "title" : titre_livre,
+                "price_including_tax" : info_livre[3],
+                "price_excluding_tax" : info_livre[2],
+                "number_available" : info_livre[5],
+                "product_description" : description_livre,
+                "category" : catagory_livre,
+                "review_rating" : book_rating,
+                "image_url" : image_couverture
+            })    
 
-            #Appel de la fonction telechargement, qui va telecharger et renomer les images
-            #Elle renvoi egalement le lien vers le dossier ou sont les images
-            chemin_image = telechargement(titre_livre, image_couverture, chemin_images)
-
-            desc_livre = [link_book, info_livre[0],titre_livre, info_livre[3], info_livre[2], info_livre[5], description_livre, catagory_livre, info_livre[6], image_couverture, chemin_image]
-            donnees_livre.append(desc_livre)
     return donnees_livre
+
+
+
+def transformed_data_books(data,chemin_image) :
+
+    # Fonction qui va transformer les données.
+    transformed_data = []
+    for book in data :
+        product_page_url = book["product_page_url"]
+        universal_product_code = book["universal_product_code (upc)"]
+        title = book["title"].replace("/", ";")
+        price_including_tax = book["price_including_tax"]
+        price_excluding_tax = book["price_excluding_tax"]
+        number_available = book["number_available"]
+        product_description = book["product_description"]
+        category = book["category"]
+        review_rating = remplacement_note(book["review_rating"])
+        image_url = book["image_url"]
+        image = chemin_image
+    
+        transformed_data.append({
+            "product_page_url" : product_page_url,
+            "universal_product_code (upc)" : universal_product_code,
+            "title" : title,
+            "price_including_tax" : price_including_tax,
+            "price_excluding_tax" : price_excluding_tax,
+            "number_available" : number_available,
+            "product_description" : product_description,
+            "category" : category,
+            "review_rating" : review_rating,
+            "image_url" : image_url,
+            "chemin_image" : image
+        })
+    return transformed_data
+
+
+
+def remplacement_note(note) :
+
+    # Fonction qui va transformer les notes récupérées en toutes lettres en chiffres.
+    remplacement = {
+        "One" : "1 sur 5",
+        "Two" : "2 sur 5",
+        "Three" : "3 sur 5",
+        "Four" : "4 sur 5",
+        "Five" : "5 sur 5"
+    }
+
+    if note in remplacement :
+        note_chiffre = remplacement[note]
+    else :
+        note_chiffre = "note non valide"
+    
+    return(note_chiffre)
